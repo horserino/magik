103424a22b082e8d6ad57a84166d71a76e57a3a0
lola <lolaristocrat@users.noreply.github.com>
2016-12-27 18:28:43 +1000

    Update magik.py

--- magik.py
+++ magik.py
@@ -1,133 +1,110 @@
-import sys
-import os
-import re
-import asyncio
-import traceback
-import requests
-import aiohttp
-import urllib.request
-import json
-import wand
-import PIL
-import copy
-import PIL.Image
-import PIL.ImageFont
-import PIL.ImageOps
-import PIL.ImageDraw
-import random
-import discord
-import subprocess
-import time
-import html
-import shutil
-import magic
+import asyncio, aiohttp, discord
+import aalib
+import os, sys, linecache, traceback, glob
+import re, json, random, math, html
+import wand, wand.color, wand.drawing
+import PIL, PIL.Image, PIL.ImageFont, PIL.ImageOps, PIL.ImageDraw
 import numpy as np
-import datetime
-import linecache
-import math
-from io import BytesIO
-from io import StringIO
-from sys import argv, path
+import cairosvg, jpglitch, urbandict
+import pixelsort.sorter, pixelsort.sorting, pixelsort.util, pixelsort.interval
+import hashlib, base64
+from vw import macintoshplus
+from urllib.parse import parse_qs
+from lxml import etree
+from imgurpython import ImgurClient
+from io import BytesIO, StringIO
 from discord.ext import commands
-from discord.message import Message
-from discord.user import User
+from utils import checks
 from pyfiglet import figlet_format
-from wand.drawing import Drawing
-from wand.image import Image
-from wand.color import Color
-from PIL import ImageDraw
-from PIL import Image
-from image import *
-from subprocess import call
+from string import ascii_lowercase as alphabet
+from urllib.parse import quote
+from mods.cog import Cog
+from concurrent.futures._base import CancelledError
 
 class magik:
     """My custom cog that does stuff!"""
 
-    def __init__(self, bot):
-        self.bot = bot
-    
-    @commands.command(pass_context=True, aliases=['imagemagic', 'imagemagick', 'magic', 'magick'])
-    async def magik(self, ctx, *urls:str):
-      """Apply magik to Image(s)\n .magik image_url or .magik image_url image_url_2"""
-      
-      try:
-        if len(urls) == 0 and len(ctx.message.attachments) == 0:
-          await self.bot.say(":no_entry: Please input url(s), mention(s) or atachment(s).")
-          return
-        xx = await self.bot.say("ok, processing")
-        img_urls = []
-        scale = None
-        content_msg = ''
-        for attachment in ctx.message.attachments:
-          img_urls.append(attachment['url'])
-        if len(ctx.message.mentions) != 0:
-          for mention in ctx.message.mentions:
-            img_urls.append(mention.avatar_url)
-        for url in urls:
-          if url.startswith('<@'):
-            continue
-          try:
-            if str(math.floor(float(url))).isdigit():
-              scale = int(math.floor(float(url)))
-              content_msg = '`Scale: {0}`\n'.format(scale)
-              if scale > 5 and ctx.message.author.id != config['ownerid']:
-                scale = 5
-                content_msg = '`Scale: {0} (Limit: <= 5)`\n'.format(scale)
-              continue
-          except:
-            pass
-          if isimage(url) == False:
-            if isgif(url):
-              await self.bot.say(":warning: `magik` is for images, pleas use gmagik!")
-            else:
-              await self.bot.say('Invalid or Non-Image!')
-            return
-          img_urls.append(url)
-        else:
-          if len(img_urls) == 0:
-            await self.bot.say(":no_entry: Please input url(s), mention(s) or atachment(s).")
-            return
-        yummy = []
-        exif = {}
-        count = 0
-        for url in img_urls:
-          b = await bytes_download(url)
-          i = wand.image.Image(file=b, format='png').clone()
-          exif.update({count:(k[5:], v) for k, v in i.metadata.items() if k.startswith('exif:')})
-          count += 1
-          i.transform(resize='800x800>')
-          if scale == None:
-            i.liquid_rescale(width=int(i.width*0.5), height=int(i.height*0.5), delta_x=1, rigidity=0)
-            i.liquid_rescale(width=int(i.width*1.5), height=int(i.height*1.5), delta_x=2, rigidity=0)
-          else:
-            i.liquid_rescale(width=int(i.width*0.5), height=int(i.height*0.5), delta_x=1, rigidity=0)
-            i.liquid_rescale(width=int(i.width*1.5), height=int(i.height*1.5), delta_x=scale, rigidity=0)
-          i.resize(i.width, i.height)
-          magikd = BytesIO()
-          i.save(file=magikd)
-          magikd.seek(0)
-          yummy.append(magikd)
-        if len(yummy) > 1:
-          imgs = [PIL.Image.open(i).convert('RGBA') for i in yummy]
-          min_shape = sorted([(np.sum(i.size), i.size) for i in imgs])[0][1]
-          imgs_comb = np.hstack((np.asarray(i.resize(min_shape)) for i in imgs))
-          imgs_comb = PIL.Image.fromarray(imgs_comb)
-          ya = BytesIO()
-          imgs_comb.save(ya, 'png')
-        else:
-          ya = yummy[0]
-        for x in exif:
-          if len(exif[x]) >= 2000:
-            continue
-          content_msg += '**Exif data for image #{0}**\n'.format(str(x+1))+code.format(exif[x])
-        else:
-          if len(content_msg) == 0:
-            content_msg = None
-        ya.seek(0)
-        await self.bot.send_file(ctx.message.channel, ya, filename='magik.png', content=content_msg)
-        await self.bot.delete_message(xx)
-      except discord.errors.Forbidden:
-        await self.bot.say(":warning: **I do not have permission to send files!**")
-    
+def do_magik(self, scale, *imgs):
+		try:
+			list_imgs = []
+			exif = {}
+			exif_msg = ''
+			count = 0
+			for img in imgs:
+				i = wand.image.Image(file=img)
+				i.format = 'jpg'
+				i.alpha_channel = True
+				if i.size >= (3000, 3000):
+					return ':warning: `Image exceeds maximum resolution >= (3000, 3000).`', None
+				exif.update({count:(k[5:], v) for k, v in i.metadata.items() if k.startswith('exif:')})
+				count += 1
+				i.transform(resize='800x800>')
+				i.liquid_rescale(width=int(i.width * 0.5), height=int(i.height * 0.5), delta_x=int(0.5 * scale) if scale else 1, rigidity=0)
+				i.liquid_rescale(width=int(i.width * 1.5), height=int(i.height * 1.5), delta_x=scale if scale else 2, rigidity=0)
+				magikd = BytesIO()
+				i.save(file=magikd)
+				magikd.seek(0)
+				list_imgs.append(magikd)
+			if len(list_imgs) > 1:
+				imgs = [PIL.Image.open(i).convert('RGBA') for i in list_imgs]
+				min_shape = sorted([(np.sum(i.size), i.size) for i in imgs])[0][1]
+				imgs_comb = np.hstack((np.asarray(i.resize(min_shape)) for i in imgs))
+				imgs_comb = PIL.Image.fromarray(imgs_comb)
+				ya = BytesIO()
+				imgs_comb.save(ya, 'png')
+				ya.seek(0)
+			elif not len(list_imgs):
+				return ':warning: **Command download function failed...**', None
+			else:
+				ya = list_imgs[0]
+			for x in exif:
+				if len(exif[x]) >= 2000:
+					continue
+				exif_msg += '**Exif data for image #{0}**\n'.format(str(x+1))+code.format(exif[x])
+			else:
+				if len(exif_msg) == 0:
+					exif_msg = None
+			return ya, exif_msg
+		except Exception as e:
+			return str(e), None
+
+	@commands.command(pass_context=True, aliases=['imagemagic', 'imagemagick', 'magic', 'magick', 'cas', 'liquid'])
+	@commands.cooldown(2, 5, commands.BucketType.user)
+	async def magik(self, ctx, *urls:str):
+		"""Apply magik to Image(s)\n .magik image_url or .magik image_url image_url_2"""
+		try:
+			get_images = await self.get_images(ctx, urls=urls, limit=6, scale=5)
+			if not get_images:
+				return
+			img_urls = get_images[0]
+			scale = get_images[1]
+			scale_msg = get_images[2]
+			if scale_msg is None:
+				scale_msg = ''
+			msg = await self.bot.send_message(ctx.message.channel, "ok, processing")
+			list_imgs = []
+			for url in img_urls:
+				b = await self.bytes_download(url)
+				if b is False:
+					if len(img_urls) > 1:
+						await self.bot.say(':warning: **Command download function failed...**')
+						return
+					continue
+				list_imgs.append(b)
+			final, content_msg = await self.bot.loop.run_in_executor(None, self.do_magik, scale, *list_imgs)
+			if type(final) == str:
+				await self.bot.say(final)
+				return
+			if content_msg is None:
+				content_msg = scale_msg
+			else:
+				content_msg = scale_msg+content_msg
+			await self.bot.delete_message(msg)
+			await self.bot.upload(final, filename='magik.png', content=content_msg)
+		except discord.errors.Forbidden:
+			await self.bot.say(":warning: **I do not have permission to send files!**")
+		except Exception as e:
+			await self.bot.say(e)
+            
 def setup(bot):
     bot.add_cog(magik(bot))
